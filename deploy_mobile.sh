b4e101cfe96b59da6b0380ace8ae9287ef223d31
Matteo Parrucci <parruc@gmail.com>
2015-10-22 15:16:26 +0200

    easy script to deploy mobile

---
+++ deploy_mobile.sh
@@ -0,0 +1,8 @@
+meteor build  ../ --server parruc_99.meteor.com
+keytool -genkey -alias 99 -keyalg RSA -keysize 2048 -validity 10000
+cd ../android
+$JAVA_HOME/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 release-unsigned.apk 99
+if [ -f 99.apk ]; then
+    rm 99.apk
+fi
+$ANDROID_HOME/build-tools/22.0.0/zipalign 4 release-unsigned.apk 99.apk
