fd984779b7b09a6a0dd246c14c2417cf2b205c0f
Matteo Parrucci <parruc@gmail.com>
2015-10-22 12:03:30 +0200

    added example app configuration

---
+++ config.js
@@ -0,0 +1,12 @@
+App.info({
+  name: '99',
+  description: 'An Android 99 game built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png',
+  'android_mdpi': 'resources/icons/icon-mdpi.png',
+  'android_hdpi': 'resources/icons/icon-hdpi.png',
+  'android_xhdpi': 'resources/icons/icon-xhdpi.png'
+});
